53aaccfdd96ed18034b9961177c4851d58aae1fc
Emre <emre140498@hotmail.com>
2024-05-24 14:18:23 +0300

    little update

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from langchain.chains import create_sql_query_chain
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 from langchain_community.utilities.sql_database import SQLDatabase
+from operator import itemgetter
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -21,9 +25,28 @@ class QueryRequest(BaseModel):
 @app.post("/query")
 async def run_query(request: QueryRequest):
     try:
-        query = generate_query.invoke({"question": request.question})
-        response = execute_query.invoke(query)
-        return {"response": response}
+        answer_prompt = PromptTemplate.from_template(
+        """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
+
+        Question: {question}
+        SQL Query: {query}
+        SQL Result: {result}
+        Answer: 
+        """
+        )
+
+        rephrase_answer = answer_prompt | llm | StrOutputParser()
+
+        chain = (
+            RunnablePassthrough.assign(query=generate_query).assign(
+                result=itemgetter("query") | execute_query
+            )
+            | rephrase_answer
+        )
+
+        result = chain.invoke({"question": request.question})
+
+        return {"response": result}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
